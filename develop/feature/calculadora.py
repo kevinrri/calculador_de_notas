9dfd45ac97ea9ac108c8f5c651ad1784c9bebc0f
kev <romariorovira@gmail.com>
2026-02-26 10:26:27 -0500

    Delete develop/feature/calculadora.py

--- develop/feature/calculadora.py
+++
@@ -1,5 +0,0 @@
-notas = input("digite sus notas separadas por una ,: ")
-notas = notas.split(",")
-notas = [float (nota) for nota in notas]
-promedio =sum(notas) / len(notas)
-print(f"el promedio de sus notas es: {promedio}")
